001e0c89074c6353c3a0912bd0b40d5da1033fba
leomatiazzz <leo21matias@hotmail.com>
2025-04-08 19:33:42 -0300

    Projeto PystackWeek 13 concluído

---
+++ mentorados/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import timedelta
+import secrets
+
+class Navigators(models.Model):
+    nome = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Mentor
+
+    def __str__(self):
+        return self.nome
+
+class Mentorados(models.Model):
+    estagio_choices = (
+        ('E1', '3k-10k'),
+        ('E2', '10k-100k'),
+        ('E3', '100k-1kk'),
+        ('E4', '1kk-10kk'),
+        ('E5', '10kk+'),
+    )
+    nome = models.CharField(max_length=255)
+    foto = models.ImageField(upload_to='fotos', null=True, blank=True)
+    estagio = models.CharField(max_length=2, choices=estagio_choices)
+    navigator = models.ForeignKey(Navigators, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado_em = models.DateField(auto_now_add=True)
+    token = models.CharField(max_length=16)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = self.gerar_token_unico()
+        super().save(*args, **kwargs)
+
+    def gerar_token_unico(self):
+        while True:
+            token = secrets.token_urlsafe(8)
+            if not Mentorados.objects.filter(token=token).exists():
+                return token
+
+    def __str__(self):
+        return self.nome
+    
+class DisponibilidadeHorarios(models.Model):
+    data_inicial = models.DateTimeField(null=True, blank=True)
+    mentor = models.ForeignKey(User, on_delete=models.CASCADE)
+    agendado = models.BooleanField(default=False)
+
+    def data_final(self):
+        return self.data_inicial + timedelta(minutes=50)
+    
+class DisponibilidadeHorarios(models.Model):
+    data_inicial = models.DateTimeField(null=True, blank=True)
+    mentor = models.ForeignKey(User, on_delete=models.CASCADE)
+    agendado = models.BooleanField(default=False)
+
+    @property
+    def data_final(self):
+        return self.data_inicial + timedelta(minutes=50)
+    
+class Reuniao(models.Model):
+    tag_choices = (
+        ('G', 'Gestão'),
+        ('M', 'Marketing'),
+        ('RH', 'Gestão de pessoas'),
+        ('I', 'Impostos'),
+        ('E', 'Empreendedorismo'),
+        ('F', 'Finanças'),
+        ('T', 'Tecnologia'),
+        ('P', 'Produto'),
+        ('V', 'Vendas'),
+        ('CS', 'Customer Success'),
+        ('D', 'Desenvolvimento Pessoal'),
+        ('N', 'Networking'),
+        ('S', 'Startup'),
+        ('L', 'Liderança'),
+        ('DA', 'Análise de Dados'),
+        ('J', 'Jurídico'),
+        ('O', 'Operações'),
+        ('X', 'Outros')
+    )
+
+    data = models.ForeignKey(DisponibilidadeHorarios, on_delete=models.CASCADE)
+    mentorado = models.ForeignKey(Mentorados, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=2, choices=tag_choices)
+    descricao = models.TextField()
+
+class Tarefa(models.Model):
+    mentorado = models.ForeignKey(Mentorados, on_delete=models.DO_NOTHING)
+    tarefa = models.CharField(max_length=255)
+    realizada = models.BooleanField(default=False)
+
+
+class Upload(models.Model):
+    mentorado = models.ForeignKey(Mentorados, on_delete=models.DO_NOTHING)
+    video = models.FileField(upload_to='video')
